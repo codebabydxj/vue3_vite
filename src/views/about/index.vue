7f722a85663bfbef26b4383acd85d8aff8f5b280
duanxiaojie <18435179812@163.com>
2025-08-25 17:46:00 +0800

    优化

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -6,7 +6,7 @@
           <h4 class="title">简介</h4>
           <span class="text">
             <el-link type="primary" @click="Router.openView(HOME_URL);">Vite-Admin</el-link>
-            一款基于 Vue3.4、TypeScript、Vite4.5、Pinia、Element-Plus 的后台管理框架，使用目前最新技术栈开发。项目提供强大的
+            一款基于 Vue3.5、TypeScript、Vite5、Pinia、Element-Plus 的后台管理框架，使用目前最新技术栈开发。项目提供强大的
             <el-link type="primary">ProTable 组件</el-link>
             在一定程度上提高您的开发效率。另外本项目还封装了一些常用组件、Hooks、指令、动态路由、按钮级别权限控制等功能。
           </span>
